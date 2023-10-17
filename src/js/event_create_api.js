880f657bb345f93e0778f11c2d22f56b0f933ae7
JocelynWong00 <jocelynflores@qq.com>
2023-10-17 23:45:42 +1000

    change the create page

--- src/js/event_create_api.js
+++ src/js/event_create_api.js
@@ -1,27 +1,50 @@
-const subscribeForm=document.getElementById('submit-form');
+// const subscribeForm=document.getElementById('submit-form');
 
-const handlerInputChange=() => {
-    let name=document.getElementById('name');
-    let location=document.getElementById('location');
-    let organiser=document.getElementById('organiser');
-    let event_type=document.getElementById('event_type');
-    let description=document.getElementById('description');
-    let date_time=document.getElementById('date_time');
-    let photo=document.getElementById('photo');
-    let website_code=document.getElementById('website_code');
-    let button=document.getElementById('submit-button')
+// const handlerInputChange=() => {
+//     let name=document.getElementById('name');
+//     let location=document.getElementById('location');
+//     let organiser=document.getElementById('organiser');
+//     let event_type=document.getElementById('event_type');
+//     let description=document.getElementById('description');
+//     let date_time=document.getElementById('date_time');
+//     let photo=document.getElementById('photo');
+//     let website_code=document.getElementById('website_code');
+//     let button=document.getElementById('submit-button')
 
-    if (name.value && location.value && organiser.value && event_type.value &&
-        description.value && date_time.value && photo.value && website_code.value)
-    {
-        button.classList.add('enabled');
-        button.disabled=false;
-    }
-    else{
-        button.classList.remove('enabled');
-        button.disabled=true;
-    }
+//     if (name.value && location.value && organiser.value && event_type.value &&
+//         description.value && date_time.value && photo.value && website_code.value)
+//     {
+//         button.classList.add('enabled');
+//         button.disabled=false;
+//     }
+//     else{
+//         button.classList.remove('enabled');
+//         button.disabled=true;
+//     }
     
+// }
+
+// subscribeForm.addEventListener('input', handlerInputChange)
+
+// global constant variables
+const photoFileInputLabel=document.getElementById('photo-file-input-label')
+const photoFileInput=document.getElementById('photo-file-input')
+
+// constant functions
+const triggerFileInput=() =>{
+    photoFileInput.click();
+}
+const handleFileChange=()=>{
+    let fileName= photoFileInput.files[0].name;
+
+    if(fileName.length>20){
+        fileName=fileName.substring(0,17)+'...'
+    }
+
+    photoFileInputLabel.textContent=fileName;
 }
 
-subscribeForm.addEventListener('input', handlerInputChange)
+// event listeners
+photoFileInputLabel.addEventListener('click', triggerFileInput);
+photoFileInput.addEventListener('change', handleFileChange);
+// page setup on first load
