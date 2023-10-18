d5971104786f14b8dcacb36af76ec41f61491093
JocelynWong00 <jocelynflores@qq.com>
2023-10-18 21:24:49 +1000

    nothing changed

--- src/js/event_create_api.js
+++ src/js/event_create_api.js
@@ -29,6 +29,14 @@
 // global constant variables
 const photoFileInputLabel=document.getElementById('photo-file-input-label')
 const photoFileInput=document.getElementById('photo-file-input')
+const my_website_code="Sihan666"
+// const myInput=document.querySelector("#date_time");
+// const fp=flatpickr(myInput,{
+//     enableTime:true,
+//     dateFormat:"Y-m-d H:i",
+// })
+const baseURLCommunityEvents="https://damp-castle-86239-1b70ee228fbd.herokuapp.com/decoapi/community_events/";
+const postCommunityEventMethod='POST';
 
 // constant functions
 const triggerFileInput=() =>{
@@ -43,8 +51,58 @@ const handleFileChange=()=>{
 
     photoFileInputLabel.textContent=fileName;
 }
+const handleFormSubmit=event=>{
+    event.preventDefault();
 
+    let formData = new FormData(event.target);
+    formData.append("website-code",my_website_code);
+    const requestOptions={
+        method:postCommunityEventMethod,
+        body:formData,
+        redirect:'follow'
+    }
+
+    fetch(baseURLCommunityEvents, requestOptions)
+    .then(response=>response.json().then(data=>{
+        if(!response.ok){
+            console.log("Server response:", data);
+            throw new Error("Network response was not ok")
+        }
+        return data;
+    }))
+    .then(data=>{
+        console.log(data);
+        alert("Event submitted successfully!")
+    })
+    .catch(error=>{
+        console.error("There was a problem with the fetch operation:", error.message);
+        alert("Error submitting event. Please try again");
+    });
+}
+// fetching events from community Events API
+const getCommunityEvents=()=>{
+    const queryParams={
+        website_code:my_website_code,
+    }
+    const queryString=new URLSearchParams(queryParams).toString();
+    const urlWithParams=baseURLCommunityEvents+"?"+queryString;
+    const requestOptions={
+        method:'GET',
+        redirect:'follow'
+    }
+    fetch(urlWithParams, requestOptions)
+    .then(response=>{
+        if(!response.ok){
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
+    }
+    )
+    .then(data=>console.log(data));
+};
 // event listeners
 photoFileInputLabel.addEventListener('click', triggerFileInput);
 photoFileInput.addEventListener('change', handleFileChange);
+eventForm.addEventListener("submit", handleFormSubmit);
+
 // page setup on first load
